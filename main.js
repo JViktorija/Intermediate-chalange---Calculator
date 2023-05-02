f53287ffb1ba2ac6fed9f40cb7ae49b9b67e9faf
Viktorija <viktorija.jankovskaja@gmail.com>
2023-05-02 18:44:52 +0300

    added mobile version

--- main.js
+++ main.js
@@ -2,11 +2,13 @@
 
 function appendToResult(value) {
   let resultElement = document.getElementById("result");
+
   if (value === "del") {
     resultElement.value = resultElement.value.slice(0, -1);
   } else {
     resultElement.value += value;
   }
+  
 }
 
 function clearResult() {
@@ -19,9 +21,10 @@ function reset() {
 
 function calculate() {
   let result = document.getElementById("result").value;
-  let answer = eval(result);
+  let answer = eval(result).toLocaleString();
   document.getElementById("result").value = answer;
 }
+
 
 // BUTTON ==============================================
 
