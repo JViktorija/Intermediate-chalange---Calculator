b423019a9d8863c122fd98b321d6ed72fda3281b
Viktorija <viktorija.jankovskaja@gmail.com>
2023-05-02 14:51:31 +0300

    button slides

--- main.js
+++ main.js
@@ -23,3 +23,31 @@ function calculate() {
   let answer = eval(result);
   document.getElementById("result").value = answer;
 }
+
+// BUTTON ==============================================
+
+const toggleInput = () => {
+  console.log("toggleInput called");
+  const inputElem = document.getElementById('toggle-input');
+  const btnBoxElem = document.getElementById('toggle-btn');
+  const currentPos = btnBoxElem.getAttribute('data-position');
+
+  switch (currentPos) {
+    case 'right':
+      btnBoxElem.style.justifyContent = 'flex-start';
+      btnBoxElem.setAttribute('data-position', 'left');
+      break;
+    case 'left':
+      btnBoxElem.style.justifyContent = 'center';
+      btnBoxElem.setAttribute('data-position', 'center');
+      break;
+    case 'center':
+      btnBoxElem.style.justifyContent = 'flex-end';
+      btnBoxElem.setAttribute('data-position', 'right');
+      break;
+    default:
+      break;
+  }
+};
+
+document.getElementById('toggle-btn').addEventListener('click', toggleInput);
